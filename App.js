f2c91bf528c18de47a9800333996be4f3ae12420
UzmaAfzal-0629 <231fa04d99@gmail.com>
2026-04-20 20:52:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App(){
+  return (
+    <div style={{padding:'20px'}}>
+      <h1>University Ticketing System</h1>
+      <p>Frontend Running Successfully</p>
+    </div>
+  );
+}
